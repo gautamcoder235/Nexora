40a871c6cbcca2cee14c857be9e23936bf26e96f
Gautam Kumar <sharmagautamkumar235@gmail.com>
2026-07-02 16:08:49 +0530

    chore: fix unused import warning

--- core/src/security/approval.rs
+++ core/src/security/approval.rs
@@ -1,7 +1,7 @@
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use std::sync::Arc;
-use tokio::sync::{RwLock, mpsc};
+use tokio::sync::RwLock;
 use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
